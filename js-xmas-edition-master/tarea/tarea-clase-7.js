b3e5b415215a617084946e7dd1dd2372b32817b3
Cyborg-Dai <cyborg.daibx@gmail.com>
2024-10-01 13:33:48 -0300

    tarea validaciones

--- js-xmas-edition-master/tarea/tarea-clase-7.js
+++ js-xmas-edition-master/tarea/tarea-clase-7.js
@@ -5,3 +5,111 @@
 * Adicional: Escribir pruebas para las funciones de tareas anteriores.
 *
 * */
+
+function validarNombre(nombre) {
+    if(nombre.length === 0) {
+        return "Este campo debe tener al menos 1 caracter";
+    }
+    if(nombre.length >= 50){
+        return "Este campo debe tener menos de 50 caracteres";
+    }if(/^[a-z]+$/i.test(nombre)) {
+        return "El campo solo debe contener letras"
+    }
+}
+
+function validarCiudad() {
+    if(ciuadad.legth === 0) {
+        return "Este campo está vacío"
+    }
+    if(nombre.length >= 20){
+        return "Este campo debe tener menos de 20 caracteres";
+    }
+    return "";
+}
+
+function validarDescripcionRegalo(descripcionRegalo) {
+    if(descripcionRegalo.length === 0) {
+        return "Este campo no puede estar vacío";
+    }
+    if(descripcionRegalo.length >= 50) {
+        return "El campo descripcion regalo debe contener menos de 50 caracteres";
+    }
+    if(/^[a-z0-9]+$/i.test(descripcionRegalo)) {
+        return "El campo descripción regalo debe contener solo numeros y letras"; // Con la i: /^[a-z0-9]+$/i coincide tanto con minúsculas como con mayúsculas (a-z o A-Z) y números (0-9). EJ: ABC123, abc123, y AbC123
+    }
+    return"";
+}
+function redirigirAWishList() {
+    window.location.href = "wishlist.html";
+}
+
+function validarFormulario(event) {
+    event.preventDefault(); //Evitar el envío del formulario por defecto
+
+    const $form = document.querySelector("carta-a-santa");
+
+    const $nombre = $form.nombre.value;
+    const $ciudad = $form.ciudad.value;
+    const $descripcionRegalo = $form["descripcion-regalo"].value; 
+
+    const errorNombre = validarNombre($nombre);
+    const errorCiudad = validarCiudad($ciudad);
+    const errorDescripcionRegalo = validarDescripcionRegalo($descripcionRegalo);
+
+    const errores = { //objeto
+        nombre: errorNombre,
+        ciudad: errorCiudad,
+        "descripcion-regalo": errorDescripcionRegalo
+    };
+
+const esExito = manejaErrores(errores) === 0;
+
+if(esExito) {
+    mostrarTextoExito();
+    ocultarFormulario();
+    const tiempoDeEspera = setTimeout(redirigirAWishList, 5000);
+}
+
+    event.preventDefault();
+}
+
+function manejarErrores(errores) {
+    const keys = Object.keys(errores);
+    const $errores = document.querySelector('#errores');
+    $errores.innerHTML = ''; //Limpiar mmensajes de errores anteriores
+    let cantidadErrores = 0;
+
+    keys.forEach(function (key) {
+        const error = errores[key];
+        const $input = document.querySelector(`[name="${key}"]`);
+
+        if (error) {
+            cantidadErrores++; //CHEQUEAR
+            $form[key].className = "error"
+
+            const $error = document.createElement('li');
+            $error.innerText = error;
+            $errores.appendChild($error);
+        }else{
+            $form(key).className = "";
+        }
+    });
+
+    return cantidadErrores;
+
+}
+
+function ocultarFormulario() {
+    const $form = document.querySelector("#carta-a-santa");
+    $form.className = "oculto";
+}
+
+function mostrarMensajeExito() {
+    const $exito=document.querySelector('#exito');
+    $exito.className = "";
+
+    
+}
+
+const $form = documentt.querySelectoor('#carta-a-santa');
+$form.onsubmit = validarFormulario;
--- js-xmas-edition-master/tarea/tarea-clase-8.js
+++ js-xmas-edition-master/tarea/tarea-clase-8.js
@@ -1,9 +1,200 @@
 /*# Tarea clase 8
 
+*Luego de mostrar EXITO, En 5 segundos vamos redireccionar al usuario a `wishlist.html`,
+  usando `setTimeout` y `window.location.href`.
+*borrar los campos adecuados segun los errores que tenga el form
+
+
 A las 2 tareas de la clase 6, ponerles las validaciones que consideren
 necesarias.
 
 TIP: Las edades no pueden tener decimales.
 */
 
+//poner que no acepte sdigno nii letras
+//poner que el campo no puede estar vvacio
+//poner que el numero de quienes trabajan no puede ser mayor a la cantidad de integrantes
 
+//Si la cantidad de integrantes se obtiene de un campo de formulario,  hacer la
+//validación al enviar el formulario o antes de continuar a la siguiente operación.
+
+
+/*# Tarea clase 8
+
+
+A las 2 tareas de la clase 6, ponerles las validaciones que consideren
+Necesarias (USANDO RegExp, Objetos, forEacch, poner estilos, mostrar los errores en la interfaz de usuario (nada de console.log)  y escribir pruebas)
+TIP: Las edades no pueden tener decimales.
+*/
+
+
+// AGREGAR A LAS DOS TAREAS DE LA CLASE 6, LAS VALIDACIONES QUE CONSIDEREMOS NECESARIA (VALIDAR EDAD, VALIDAR NOMBRE) TODO CON REGULAR EXPRESION, CON OBJETOS,
+//  FOREACH Y VAMOS A ESCRIBIR PRUEBAS 
+
+// VAMOS A COPIAR DE LOS ARCHIVOS ORIGINALES Y LO VAMOS A PONER DENTRO DE ESTE OTRO REPOSITORIO NUEVO Y LO VAMOS A IR SUBIENDO AHÍ. LO SUBIMOS A GIHUB Y VAMOS 
+// ARMANDO UN PULL REQUEST
+
+
+function validarCantidadIntegrantes() {
+    cantidadIntegrantes = Number(cantidadIntegrantes);
+    //Validar que no sea 0
+    if (cantidadIntegrantes === 0) {
+        return "La cantidad de integrantes no puede ser 0";
+    }
+    // Validar que sea un número positivo
+    if (cantidadIntegrantes < 0) {
+        return "La cantidad de integrantes no puede ser un número negativo"
+    }
+    //Validar que no tenga más de 3 dígitos
+    if (String(cantidadIntegrantes).length > 3){
+        return "La cantidad de integrantes no puede tener más de 3 dígitos";
+    }
+    //validar que solo contenga números (sin letras ni símbolos)
+    if(!/^[0-9])+$/.test(cantidadIntegrantes)) {
+        return "La cantidad de integrantes debe contener solo números, sin letras ni símbolos";
+    }
+
+    return "";
+    
+}
+
+
+function validarEdadIntegrantes() {
+    edadIntegrantes = Number(cantidadIntegrantes);
+
+    if (edadIntegrantes === 0) {
+        return "La cantidad de integrantes no puede ser 0";
+    }
+
+    if (edadIntegrantes < 0) {
+        return "La cantidad de integrantes no puede ser un número negativo"
+    }
+
+    if (String(edadIntegrantes).length > 3){
+        return "La cantidad de integrantes no puede tener más de 3 dígitos";
+    }
+
+    if(!/^[1-9][0-9]{0,2}$/.test(edadIntegrantes)) {
+        return "La cantidad de integrantes debe contener solo números, sin letras ni símbolos y no puede tener decimales";
+    }
+
+    return "";
+    }
+
+function validarCantidadSalarios(cantidadSalario) {
+    cantidadSalario = Number(cantidadSalario);
+
+    if (cantidadSalario === 0) {
+        return "La cantidad de integrantes no puede ser 0";
+    }
+
+    if (cantidadSalario < 0) {
+        return "La cantidad de integrantes no puede ser un número negativo"
+    }
+
+    if (String(cantidadSalario).length > 3) {
+        return "La cantidad de integrantes no puede tener más de 3 dígitos";
+    }
+
+    if (!/^[0-9])+$/.test(String(cantidadSalario))) {
+        return "La cantidad de integrantes debe contener solo números, sin letras ni símbolos";
+    }
+
+    return "";
+
+    }
+
+
+function validarSalarioIntegrante(salarioIntegrante) {
+    salarioIntegrante = Number(salarioIntegrante);
+
+    if (salarioIntegrante === 0) {
+        return "El salario no puede ser 0";
+    }
+
+    if (salarioIntegrante < 0) {
+        return "El salario no puede ser un número negativo"
+    }
+
+    if (String(salarioIntegrante).length > 7) {
+        return "El salario no puede tener más de 7 dígitos";
+    }
+
+    if (!/^[0-9])+$/.test(String(salarioIntegrante))) {
+        return "El salario debe contener solo números, sin letras ni símbolos";
+    }
+
+    return "";
+    }
+
+function validarFormularioEdades(event) {
+    event.preventDefault();
+
+    const $form = document.querySelector("#calculador-edades");
+
+    const $edad = $form.edad.value; //chequear nombre de id 
+
+    const errorEdad = validarEdadIntegrantes($edad);
+
+    const errores = {
+        edad: errorEdad;
+    };
+
+    const esExito = manejaErrores (errores) === 0;
+
+    if(esExito) {
+        mostrarTextoExito();
+    }
+
+    event.preventDefault();
+}
+
+function manejarErrores(errores) {
+    const keys = Object.keys(errores);
+    const $errores = document.querySelector('#errores');
+    $errores.innerHTML = ''; //Limpiar mmensajes de errores anteriores
+    let cantidadErrores = 0;
+
+    keys.forEach(function (key) {
+        const error = errores[key];
+        const $input = document.querySelector(`[name="${key}"]`);
+
+        if (error) {
+            cantidadErrores++; //CHEQUEAR
+            $form[key].className = "error"
+
+            const $error = document.createElement('li'); //chequear
+            $error.innerText = error; 
+            $errores.appendChild($error);
+        }else{
+            $form(key).className = "";
+        }
+    });
+
+    return cantidadErrores;
+
+}
+
+function validarFormularioSalario(event) {
+    event.preventDefault();
+
+    const $form = document.querySelector("#calculador-edades");
+
+    const $edad = $form.edad.value; //chequear nombre de id 
+
+    const errorEdad = validarEdadIntegrantes($edad);
+
+    const errores = {
+        edad: errorEdad;
+    };
+
+    const esExito = manejaErrores (errores) === 0;
+
+    if(esExito) {
+        mostrarTextoExito();
+    }
+
+    event.preventDefault();
+}
+
+"calculador-salario"
